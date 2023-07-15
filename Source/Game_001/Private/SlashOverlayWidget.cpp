615ecbd7cb149a706049aedf04758d6726b6ecb1
Saish-in3d <123372125+Saish-in3d@users.noreply.github.com>
2023-07-15 19:39:08 +0530

    Added Slash overlay binding with c++

---
+++ Source/Game_001/Private/SlashOverlayWidget.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SlashOverlayWidget.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
+void USlashOverlayWidget::SetHealthBarPercent(float Percent)
+{
+	if (HealthProgressBar)
+	{
+		HealthProgressBar->SetPercent(Percent);
+	}
+}
+
+void USlashOverlayWidget::SetStaminaBarPercent(float Percent)
+{
+	if (StaminaProgressBar)
+	{
+		StaminaProgressBar->SetPercent(Percent);
+	}
+}
---
+++ Source/Game_001/Public/SlashOverlayWidget.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SlashOverlayWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME_001_API USlashOverlayWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	void SetHealthBarPercent(float Percent);
+	void SetStaminaBarPercent(float Percent);
+
+
+private:
+	UPROPERTY(meta = (BindWidget))
+		class UProgressBar* HealthProgressBar;
+
+	UPROPERTY(meta = (BindWidget))
+		class UProgressBar* StaminaProgressBar;
+
+
+	
+};
